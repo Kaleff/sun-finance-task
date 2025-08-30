e082021887fa54540d39477b2f93803f367ce33c
Marks Kalijevs <markskalijevs@gmail.com>
2025-08-31 02:23:16 +0300

    Remove unused tokens table and user route in api

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ use App\Http\Controllers\PaymentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/payment/store', [PaymentController::class, 'store']);
